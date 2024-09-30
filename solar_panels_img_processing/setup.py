5c26f459a6d1ca8aba967bb16b79f42e45ee75b2
Marcos Moura <gabbyru2@gmail.com>
2024-09-30 21:03:26 +0200

    feat: add launch folder

--- solar_panels_img_processing/setup.py
+++ solar_panels_img_processing/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'solar_panels_img_processing'
 submodules = 'solar_panels_img_processing.utils'
@@ -11,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -22,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'canny_node = solar_panels_img_processing.canny_node:main',
+            'dsef_node = solar_panels_img_processing.dsef_node:main',
             'georef_node = solar_panels_img_processing.georef_node:main',
         ],
     },
